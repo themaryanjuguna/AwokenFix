799214325e1cb03e8a04e361f15be63bdf4b9044
themaryanjuguna <themaryanjugunaagmail.com>
2022-05-30 16:35:37 +0300

    create a Department blueprint with getters and setters

---
+++ src/main/java/models/Department.java
@@ -0,0 +1,48 @@
+package models;
+
+public class Department {
+    public int id;
+    private String name;
+    private String profile;
+    private int deptId;
+
+
+    //constructor
+    public Department(String name, String profile, Integer deptId) {
+        this.name = name;
+        this.profile = profile;
+        this.deptId = deptId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public int getDeptId() {
+        return deptId;
+    }
+
+    public void setDeptId(int deptId) {
+        this.deptId = deptId;
+    }
+}
